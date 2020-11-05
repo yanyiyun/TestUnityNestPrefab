e4a7caf9373dcbb656d6b356fba010827d62f911
yanyy <yanyy@taojinhudong.com>
2020-11-05 14:06:41 +0800

    first commint

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class Builder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [MenuItem("Tools/Build AssetBundles")]
+    static public void BuildAssetBundles()
+    {
+        string path = "assetbundles";
+        new DirectoryInfo(path).Create();
+        AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, EditorUserBuildSettings.activeBuildTarget);
+
+        Debug.Log("Build Ok!");
+    }
+
+
+}
